a90fdbd6763cd720abfba7bce7820d054f2e989a
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-10-22 21:19:40 +0300

    Add verbose mode

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 model = "gemini-2.0-flash-001"
 
-
 def main():
-    print("Hello from coding-agent!")
+    # Command line arguments
+    verbose = False
+    user_prompt = ""
 
     if len(sys.argv) == 1:
         print("No prompt given, exiting.")
         sys.exit(1)
+        
+    for arg in sys.argv:
+        if arg == "--verbose":
+            verbose = True
+        else:
+            user_prompt = sys.argv[1]
+         
+    # Chat
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
-    prompt = sys.argv[1]
-
-    response = client.models.generate_content(model=model, contents=prompt)
+    response = client.models.generate_content(
+        model=model,
+        contents=messages
+    )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose: 
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
